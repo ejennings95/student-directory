94dc33702158c9851ee79e7bb6aa80aa5c0894b2
Elliot Jennings <elliot.r.jennings@outlook.com>
2018-12-12 14:46:05 +0000

    Introduction of methods in directory

--- directory.rb
+++ directory.rb
@@ -1,22 +1,37 @@
 # first we need tyo create an array containing the students names
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
-]
-# now lets print them
-puts "The students of Villains Academy"
-puts "------------------"
-students.each do |name|
-  puts name
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+  ]
+
+# create the method print_header
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------------"
 end
-# finally we print the number of students we have
-puts "Overall, we have #{students.count} great students"
+
+# create the method print
+def print(names)
+  names.each do |name|
+    puts name
+end
+end
+
+# create the method print_footer
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# call the methods:
+print_header
+print(students)
+print_footer(students)
