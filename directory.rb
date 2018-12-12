55d3a535a0f45bac668ecb0c63c794c51839a818
Elliot Jennings <elliot.r.jennings@outlook.com>
2018-12-12 14:13:43 +0000

    Directory - remove repetition and separate the data from the operation

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# first we puts a list of the students
+# first we need tyo create an array containing the students names
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+# now lets print them
 puts "The students of Villains Academy"
 puts "------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally we print the number of students we have 
-puts "Overall, we have #{student_count} great students"
+students.each do |name|
+  puts name
+end
+# finally we print the number of students we have
+puts "Overall, we have #{students.count} great students"
