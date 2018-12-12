e5d550cd0fa4801416a913b8357b8fa9e8ef4820
Elliot Jennings <elliot.r.jennings@outlook.com>
2018-12-12 11:51:15 +0000

    Student count added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we puts a list of the students
 puts "The students of Villains Academy"
 puts "------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then we need to print the number of students we have
+print "Overall, we have "
+# we are using print here so it doesn't add a new line
+print "11"
+puts " great students"
