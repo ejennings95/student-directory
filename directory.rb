85565b00d3db8fc75eba0c0f18dd5bcc950875db
Elliot Jennings <elliot.r.jennings@outlook.com>
2018-12-20 13:13:03 +0000

    Exercise 5 - added in date of birth and sport of choice for each student

--- directory.rb
+++ directory.rb
@@ -1,22 +1,39 @@
 def input_students
-# here we are about to ask for the names and give an instruction on how to get out the while loop that will follow
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+# here we are about to ask for the student detials and give an instruction on how to get out the while loop that will follow
+  puts "Please enter the student's details when prompted."
+  puts "To finish, type 'completed'."
 # create an empty array, this allows us to store the information we gather from the user
   students = []
-# get the first name using ‘gets’ and .chomp to remove the line drop
+# get the first name using ‘gets’ and .chomp to remove the line drop, if completed end. Otherwise continue through more details
+  puts "Please enter the name of the student:"
   name = gets.chomp
-# while the name is not(!) empty, repeat this code
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-# above shows us we are hardcoding all but the name that we are given and using the shovel operator to push the data into our empty array
-    puts "Now we have #{students.count} students."
-# ask for the next name
-    name = gets.chomp
-# this is taking another name to be added to our hash and then subsequently our array. If it is left empty then the loop is broken
+  if name != "completed"
+    puts "Please enter the date of birth of the student (dd/mm/yy):"
+    birthday = gets.chomp
+    puts "Please enter the student's sport of choice:"
+    sport = gets.chomp
+# as the name was not 'completed' we need the while loop to keep asking and store the information in our array
+    while true do
+      students << {name: name, birthday: birthday, sport: sport, cohort: :november}
+# above shows us we are hardcoding all but the name, birthday and sport that we are given. we are using the shovel operator to push the data into our empty array
+# now we need to put the student count
+      puts "Now we have #{students.count} students."
+# ask for the next name, birthday and sport. if name has our get out clause of 'completed' then end
+      puts "Please enter the name of the student:"
+      name = gets.chomp
+      if name == "completed"
+        break
+      elsif name != "completed"
+        puts "Please enter the date of birth of the student (dd/mm/yy):"
+        birthday = gets.chomp
+        puts "Please enter the student's sport of choice:"
+        sport = gets.chomp
+      else
+      end
+    end
   end
 # return the students
-  students
+students
 end
 
 # create the method print_header
@@ -30,13 +47,13 @@ def print(names)
   count = 1
   while count <= names.count
     names.each do |name|
-      puts "#{name[:name]} (#{name[:cohort]} cohort)"
+      puts "#{name[:name]}, date of birth: #{name[:birthday]}, sport of choice: #{name[:sport]} (#{name[:cohort]} cohort)"
       count += 1
     end
   end
 end
 
-# create the method print_footer
+# create the method print_footer with counter
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
