8d0bf4c2c18cc0e7133225bf80b1648b8a47e958
Elliot Jennings <elliot.r.jennings@outlook.com>
2018-12-20 19:00:09 +0000

    Interactive menu and refactor

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 $cohort_months = [
   'january',
   'february',
@@ -16,55 +17,79 @@ $cohort_months = [
 $width = 100
 
 def input_students
-
   puts "Please enter the student's details when prompted.".center($width)
   puts "To finish, type 'completed'.".center($width)
-
-  students = []
-
-
   puts "1) Please enter the name of the student:"
   name = gets.strip
 
-    while true do
+  while true do
+    if name.empty?
+      name = "New Starter"
+    end
 
-      if name.empty?
-        name = "New Starter"
-      end
-
-      if name == "completed"
-        break
-      elsif name != "completed"
-        puts "2) Please enter the date of birth of the student (dd/mm/yy):"
-        birthday = gets.strip
+    if name == "completed"
+      break
+    elsif name != "completed"
+      puts "2) Please enter the date of birth of the student (dd/mm/yy):"
+      birthday = gets.strip
         if birthday.empty?
           birthday = "tbc"
         end
-        puts "3) Please enter the student's sport of choice:"
-        sport = gets.strip
+      puts "3) Please enter the student's sport of choice:"
+      sport = gets.strip
         if sport.empty?
           sport = "tbc"
         end
-        puts "4) Please enter the student's cohort start month"
-        while true do
-          cohort = gets.strip
-          break if $cohort_months.include?(cohort)
-          puts "Please enter a correct month"
-        end
+      puts "4) Please enter the student's cohort start month"
+      while true do
+        cohort = gets.strip
+        break if $cohort_months.include?(cohort)
+        puts "Please enter a correct month"
+      end
+    end
 
+    @students << {name: name, birthday: birthday, sport: sport, cohort: cohort}
+      if @students.count == 1
+        puts "Now we have #{@students.count} student."
+      else
+        puts "Now we have #{@students.count} students."
       end
 
-      students << {name: name, birthday: birthday, sport: sport, cohort: cohort.to_s}
-        if students.count == 1
-          puts "Now we have #{students.count} student."
-        else
-          puts "Now we have #{students.count} students."
-        end
+    puts "1) Please enter the name of the student:"
+    name = gets.strip
+  end
+end
 
-      puts "1) Please enter the name of the student:"
-      name = gets.strip
-    end
- students
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1 - Input the students"
+  puts "2 - Show the students"
+  puts "9 - Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "Sorry, I do not know this command."
+  end
 end
 
 def print_header
@@ -72,60 +97,38 @@ def print_header
   puts "------------------".center($width)
 end
 
-def print(names)
+def print_student_list
   monthhash = {}
   count = 1
 
-      names.each do |student|
-      start_month = student[:cohort]
+  @students.each do |student|
+    start_month = student[:cohort]
 
-        if monthhash[start_month] == nil
-            monthhash[start_month] = []
-        end
+      if monthhash[start_month] == nil
+          monthhash[start_month] = []
+      end
+    monthhash[start_month] << "#{student[:name]} - date of birth: #{student[:birthday]}, sport of choice: #{student[:sport]} (#{student[:cohort]} cohort)"
+  end
 
-        monthhash[start_month] << "#{student[:name]} - date of birth: #{student[:birthday]}, sport of choice: #{student[:sport]} (#{student[:cohort]} cohort)"
-
-        end
-        $cohort_months.each do |months|
-          monthhash.each do |month, info|
-           if months == month
-          puts "#{month.capitalize}:".center($width)
-          info.each do |pupil|
-            puts "#{count}. #{pupil}".center($width)
+  $cohort_months.each do |months|
+    monthhash.each do |month, info|
+      if months == month
+        puts "#{month.capitalize}:".center($width)
+        info.each do |pupil|
+          puts "#{count}. #{pupil}".center($width)
           count += 1
         end
-        end
-        end
-end
-end
-
-
-
-
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student."
-  else
-    puts "Overall, we have #{names.count} great students."
-  end
-end
-
-def less_than_12(names)
-  puts "These students have less than 12 characters in there name:".center($width)
-  names.each do |name|
-    full_name = name[:name]
-    full_name_no_space = full_name.gsub(" ", "")
-    if full_name_no_space.length <= 12
-      puts " - #{full_name}"
+      end
     end
   end
 end
 
-students = input_students
-if students.empty?
-else
-  print_header
-  print(students)
-  print_footer(students)
-  less_than_12(students)
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student."
+  else
+    puts "Overall, we have #{@students.count} great students."
+  end
 end
+
+interactive_menu
